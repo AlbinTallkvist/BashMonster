765e728f84cb5c12ee02b799c8c898d8bc16c0ee
Albin Tallkvist <albin.tallkvist@elev.ga.ntig.se>
2023-09-01 11:58:59 +0200

    Perfekt, just perfekt.

--- BashMonster/Program.cs
+++ BashMonster/Program.cs
@@ -6,14 +6,13 @@ bool vinst = false;
 
 // Introducera spelet, skriv HP av spelare och fienden
 System.Console.WriteLine("Welcome till fighting spelet, vänligen börja med att atackera fienden!");
-System.Console.WriteLine("Fienden har 100hp"); //Gör om så uppdateras varje runda
-System.Console.WriteLine("Du har 0 swagness)"); // Gör så uppdateras varje runda
 
 while (!vinst) 
 {
     // Starta första rundan och attackera monstret
     System.Console.WriteLine("Attackera nu! (Tryck Enter för att attackera)");
-    System.Console.WriteLine("Fienden har nu X hp kvar och du är X % swag"); // remembr hur man gjord uppdaterande nu igen
+    System.Console.WriteLine($"Fienden har nu {Fienden.hp} hp kvar och du är {Spelaren.swagness} % swag"); // remembr hur man gjord uppdaterande nu igen
+
     System.Console.ReadLine(); // 
 
 // Ta bort hp från monster
